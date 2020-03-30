b1edc44209b3ffafe4e302dd7a7840ea37c64896
jeg91359 <jeg91359@yahoo.com>
2020-03-30 12:07:20 -0500

    Update index.js
    Create Comments endpoint configuration

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,30 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(),
+  databaseURL: "ws://fs1prod-27deb.firebaseio.com/"
+});
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send("Hello from Firebase!");
+});
+
+exports.getComments = functions.https.onRequest((request, response) => {
+  response.send("Comments from Firebase!");
+});
+
+exports.createComments = functions.https.onRequest((request, response) => {
+    console.log(request.body);
+  admin
+    .database()
+    .ref("/comments")
+    .push(request.body);
+  response.send("Comments added from Firebase!");
+});
+
+
